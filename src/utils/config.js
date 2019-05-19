456579d13f8252051ad82e76a45aa56bebe33d4a
yangdoubleming <yangdoubleming@163.com>
2019-05-19 15:31:18 +0800

    接口地址注释

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-const BASE_URL='http://192.168.1.158:8082'  //(此处书写需要复用的url，注意写上端口号)RL
-const IMG_URL='http://192.168.1.158:8000'
+const BASE_URL='http://192.168.1.158:8082'  //接口地址
+const IMG_URL='http://192.168.1.158:8000'  // 图片地址
 export{ BASE_URL, IMG_URL}
